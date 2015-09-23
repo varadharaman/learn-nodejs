e995867b108bfa1876454459efdc9f45cfb6f673
Varadha <varadharaman@live.com>
2015-09-22 23:20:30 -0700

    jade

---
+++ testExpressApp/express-view-demo.js
@@ -0,0 +1,14 @@
+var express=require('express');
+var http=require('http');
+var app=express();
+
+app.set('views','./views');
+app.set('view engine','jade');
+
+app.get('/', function(req, res) {
+	res.render('jadedemo', {pageTitle:'Learning Jade', youAreUsingJad: true});
+});
+
+http.createServer(app).listen(8000, function() {
+	console.log('App started');
+});
